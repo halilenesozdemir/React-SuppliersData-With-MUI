98377ace669c1cad5e7ace43d4f1c57d170ba4e5
Halil Enes <halil.enes.ozdemir@gmail.com>
2022-06-16 01:44:15 +0300

    Add Category example  repeated

--- src/fetchSample/AddCategory.js
+++ src/fetchSample/AddCategory.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 
 const AddCategory = () => {
   const [name, setName] = useState('');
-  const [description, setDescription] = useState('');
+  const [description, setDescription] = useState();
 
   const addNewCategory = () => {
     let newCategory = {
@@ -22,25 +22,24 @@ const AddCategory = () => {
 
     fetch('https://northwind.vercel.app/api/categories', requestOptions)
       .then(res => res.json())
-      .then(data => {
-        console.log('Data', data);
-      });
+      .then(data => console.log('Data', data));
   };
 
   return (
     <>
       <div>
-        <label htmlFor=''>Name</label>
-        <input type='text' onChange={e => setName(e.target.value)} placeholder='name' />
+        <label>Name</label>
+        <input type='text' onChange={e => setName(e.target.value)} placeholder='Name' />
       </div>
 
       <div>
-        <label htmlFor=''>Description</label>
-        <input type='text' onChange={e => setDescription(e.target.value)} placeholder='Description'></input>
+        <label> Description </label>
+        <input type='text' onChange={e => setDescription(e.target.value)} placeholder='Description' />
       </div>
 
       <button onClick={() => addNewCategory()}>Add</button>
     </>
   );
 };
+
 export default AddCategory;
