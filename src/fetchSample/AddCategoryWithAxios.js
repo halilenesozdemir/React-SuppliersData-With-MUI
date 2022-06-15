8b0795247a5b5e5666dd36430f40a1c7b583b3fc
Halil Enes <halil.enes.ozdemir@gmail.com>
2022-06-16 01:51:37 +0300

    Add Category with Axios has completed.

---
+++ src/fetchSample/AddCategoryWithAxios.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import React from 'react';
+import { useState } from 'react';
+
+const AddCategory = () => {
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState();
+
+  const addNewCategory = () => {
+    let newCategory = {
+      name: name,
+      description: description,
+    };
+
+    axios.post('https://northwind.vercel.app/api/categories', newCategory).then(res => console.log('Axios Response,', res));
+  };
+
+  return (
+    <>
+      <div>
+        <label>Name</label>
+        <input type='text' onChange={e => setName(e.target.value)} placeholder='Name' />
+      </div>
+
+      <div>
+        <label> Description </label>
+        <input type='text' onChange={e => setDescription(e.target.value)} placeholder='Description' />
+      </div>
+
+      <button onClick={() => addNewCategory()}>Add</button>
+    </>
+  );
+};
+
+export default AddCategory;
