db1b676bec91a00ab364ce06441c25da18742482
Halil Enes <halil.enes.ozdemir@gmail.com>
2022-06-16 02:01:01 +0300

    Jsx Sample has completed.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import EffectSample1 from './effectSample/EffectSample1';
 import AddCategory from './fetchSample/AddCategory';
 import AddCategoryWithAxios from './fetchSample/AddCategoryWithAxios';
 import UserList from './fetchSample/UserList';
+import UsersTable from './fetchSample/UsersTable';
 import CategoryTable from './stateSample/CategoryTable';
 
 function App() {
-  return <AddCategoryWithAxios />;
+  return <UsersTable />;
 }
 
 export default App;
