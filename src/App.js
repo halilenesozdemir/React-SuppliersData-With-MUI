ebaf563e58d68ce4a2a4ffc71e5d5e43eb85d824
Halil Enes <halil.enes.ozdemir@gmail.com>
2022-06-16 16:48:14 +0300

    React Router starting file created, ı'm so excited :)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import SupplierList from './propsSample/SupplierList';
+import { Route, Routes } from 'react-router-dom';
+import AboutPage from './routerSample/AboutPage';
+import ContactPage from './routerSample/ContactPage';
+import HomePage from './routerSample/HomePage';
 
 const App = () => {
   return (
     <>
-      <SupplierList></SupplierList>
+      <h1>Site Header</h1>
+      <Routes>
+        <Route path='/' element={<HomePage />}></Route>
+        <Route path='/hakkimizda' element={<AboutPage />}></Route>
+        <Route path='iletisim' element={<ContactPage />}></Route>
+      </Routes>
+      <h1>Site Footer</h1>
     </>
   );
 };
