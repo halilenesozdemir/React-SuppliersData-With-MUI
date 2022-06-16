c6382c7318950f49fdbaf4d29fe8677245adc6e0
Halil Enes <halil.enes.ozdemir@gmail.com>
2022-06-16 16:00:56 +0300

    parent to child method transferred logic understood.

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import AddCategory from './fetchSample/AddCategory';
 import AddCategoryWithAxios from './fetchSample/AddCategoryWithAxios';
 import UserList from './fetchSample/UserList';
 import ProductDetail from './propsSample/ProductDetail';
+import PropClickEventSample from './propsSample/PropClickEventSample';
 import TechButton from './propsSample/TechButton';
 import CategoryTable from './stateSample/CategoryTable';
 
 function App() {
+  const hola = () => {
+    alert('Hola');
+  };
   return (
     <>
-      <div>Form</div>
-      <TechButton title='Add' width={75} height={75}></TechButton>
+      <PropClickEventSample hello={hola}></PropClickEventSample>
     </>
   );
 }
