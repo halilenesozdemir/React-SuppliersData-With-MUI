a176d24aad1d8e53ad3a02885073c01e7a17002b
Halil Enes <halil.enes.ozdemir@gmail.com>
2022-06-16 20:06:55 +0300

    DataGrid mui has completed.

--- src/materialSample/ProductDataGrid.js
+++ src/materialSample/ProductDataGrid.js
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { DataGrid } from '@mui/x-data-grid';
 
 const ProductDataGrid = () => {
-  return <div>ProductDataGrid</div>;
+  const [products, setProducts] = useState([]);
+
+  const columns = [
+    {
+      field: 'id',
+      headerName: 'Product Id',
+      width: 150,
+    },
+    {
+      field: 'name',
+      headerName: 'Product Name',
+      width: 150,
+    },
+    {
+      field: 'unitPrice',
+      headerName: 'Price',
+      width: 150,
+    },
+    {
+      field: 'unitsInStock',
+      headerName: 'Stock',
+      width: 150,
+    },
+  ];
+
+  useEffect(() => {
+    axios.get('https://northwind.vercel.app/api/products').then(res => setProducts(res.data));
+  }, []);
+
+  return (
+    <>
+      <div style={{ height: 400, width: '100%' }}>
+        <DataGrid columns={columns} rows={products} pageSize={6} checkboxSelection></DataGrid>
+      </div>
+    </>
+  );
 };
 
 export default ProductDataGrid;
